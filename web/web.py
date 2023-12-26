ce40a8c967682ab3500ea81b6fd89cacc6372b70
Thanathip Limna <boatkrap@gmail.com>
2023-12-26 09:55:42 +0700

    add sample post method

--- web/web.py
+++ web/web.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", name="Boat")
+    return render_template("index.html", name=request.args.get("name"))
 
 
 @app.route("/me")
 def me():
     return "Hello World"
+
+
+@app.route("/write")
+def write():
+    print("write -->", request.form)
+    return render_template("write.html")
+
+
+@app.route("/save", methods=["POST"])
+def save():
+    print("save -->", request.form)
+    return redirect(url_for("index", name=request.form.get("message", "No message")))
