dc19b3330b84b5810f65ce0071aa21e9bb5c7f13
Thanathip Limna <boatkrap@gmail.com>
2023-12-27 10:47:32 +0700

    add sqlalchemy

--- web/web.py
+++ web/web.py
@@ -1,7 +1,42 @@
 from flask import Flask, render_template, redirect, url_for, request
 
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
 
+import datetime
+
+class Base(DeclarativeBase):
+  pass
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(db.String, unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(db.String)
+
+class Group(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    created_date: Mapped[datetime.datetime] = mapped_column(db.DateTime)
+
+class Message(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    message: Mapped[str] = mapped_column(db.String)
+
+
+
+
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def index():
